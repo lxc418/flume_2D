813f95188510e6c12f88fdd8df5ac60d93faaae8
Mr Xiaocheng Liu <s4524462@goliath.labs.eait.uq.edu.au>
2021-06-04 18:03:05 +1000

    modified for video and plot

--- modelling_benchmark/sl_figure.m
+++ modelling_benchmark/sl_figure.m
@@ -7,17 +7,19 @@ time_step = length(bcof);
 time_day  = [bcof.tout]/3600/24;%second to day
 time_nod_day= arrayfun(@(y) y.tout,nod) * c.dayPsec;
 
-x_matrix = reshape(nod(1).terms{x_idx},[inp.nn1,inp.nn2]);
+x_matrix = reshape(nod(1).terms{x_idx},[inp.nn1,inp.nn2]);%inp.nn2 is number of nodes in y direction 
 y_matrix = reshape(nod(1).terms{y_idx},[inp.nn1,inp.nn2]);
+x_ele_matrix = reshape(ele(1).terms{xele_idx},[inp.nn1-1,inp.nn2-1]);
+y_ele_matrix = reshape(ele(1).terms{yele_idx},[inp.nn1-1,inp.nn2-1]);
 
 %% evaporation data
-evapo_kgs = zeros(inp.nn1,time_step);
-for i=1:inp.nn1
+evapo_kgs = zeros(inp.nn2,time_step);
+for i=1:inp.nn2
     
-if i<inp.nn1   
-    area1(1:i)    = (x_matrix(1,i+1)-x_matrix(1,i))*inp.z(1); %evaporation area 
+if i<inp.nn2   
+    area1(1:i)    = (x_matrix(1,i+1)-x_matrix(1,i)); %evaporation area 
 else
-    area1(1:i)    = (x_matrix(1,i)-x_matrix(1,i-1))*inp.z(1); %the right end node
+    area1(1:i)    = (x_matrix(1,i)-x_matrix(1,i-1)); %the right end node
 end 
 
     evapo_kgs(i,:)  = -arrayfun(@(y) y.qin(i),bcof);
@@ -35,44 +37,36 @@ for i=2:time_step
     cumulative_evapo_mm(i) =  total_evapo_mmday(i)*inp.scalt*inp.nbcfpr*c.dayPsec + cumulative_evapo_mm(i-1);
 end
 
-%% plot
-fig_pos.left   = 0.3;
+
+%% plot control
+fig_pos.left   = 0.05;
 fig_pos.bottom = 0.7;
-fig_pos.length = 0.4;
-fig_pos.height = 0.25;
+fig_pos.length = 0.35;
+fig_pos.height = 0.26;
 
 %nt=10;
 %a.fig=figure;
 a.fs = 10;
 a.lw = 2; %line width
 a.cz = 8; %the size of the marker
-fs   = 5; % sampling frequency
+fs   = 2; % sampling frequency
 % Creating the movie : quality = 100%, no compression is used and the
 % timing is adjusted to the sampling frequency of the time interval
 qt = 100;
 %A number from 0 through 100. Higher quality numbers result in higher video quality and larger file sizes
 a.fig = figure;
-% set(gcf,'Units','normalized', 'WindowStyle','docked','OuterPosition',[0 0 1 1]);  % maximize the plotting figure
-set(gcf,'Units','normalized', 'OuterPosition',[0 0 1 1]);  % maximize the plotting figure
+set (gcf,'Position',[0,0,1600,1200]);
+% set(gcf,'Units','normalized', 'OuterPosition',[0 0 1 1]);  % maximize the plotting figure
 mov           =  VideoWriter('linux.avi');% avifile('pvc1.avi','quality',qt,'compression','indeo5','fps',fs);
 mov.FrameRate = 5;
 mov.Quality=qt;
 open(mov);
 
 for nt=1:round(time_step/50):time_step
-
-    % fprintf('plotting the %i out of %i results,\n', nt, length(nod));
-    % number_of_negative_c=sum(nod(nt).terms{c_idx}<0);
-    % [max_c,max_c_idx]=max(nod(nt).terms{c_idx});
-    % fprintf('    max concentration is %f at location  %i, x= %f , y= %f \n',max_c,max_c_idx,  x_nod_mtx(max_c_idx),  y_nod_mtx(max_c_idx));
-    % if number_of_negative_c>0;
-          % fprintf('    %i negative concentration\n', length(number_of_negative_c));
-    % end
-
     %% -------------  sub 1 ET over time  --------------
     a.sub1=subplot('position'...
          ,[fig_pos.left,fig_pos.bottom,...
-          fig_pos.length,fig_pos.height]);
+          fig_pos.length-0.035,fig_pos.height]);
 yyaxis left
     a.plot1=plot(time_day(1:nt),total_evapo_mmday(1:nt),...
              'k-','linewidth',a.lw);hold on
@@ -80,7 +74,7 @@ yyaxis left
     get(gca,'xtick');
     set(gca,'fontsize',10);
     ylabel('Evt (mm/day)','FontSize',a.fs);
-    axis([-0.1 time_day(end) -0.5 2000])
+    axis([-0.1 time_day(end) -0.5 inf])
 yyaxis right
     a.plot1=plot(time_day(1:nt),cumulative_evapo_mm(1:nt),...
              'b--','linewidth',a.lw);hold off
@@ -91,35 +85,49 @@ yyaxis right
     title(txt);
     xlabel('Time (day)','FontSize',a.fs);
     ylabel('Cumulative Evt (mm)','FontSize',a.fs);
-    axis([-0.1 time_day(end) -0.5 100])
+    axis([-0.1 time_day(end) 0 inf])
 
     %title('ead profile');
 
-    %% -------------  sub 2 ET for each node  --------------
+    %% -------------  sub 2 ET for all surface nodes  --------------
     a.sub2=subplot('position'...
-         ,[fig_pos.left,fig_pos.bottom-0.3,...
-          fig_pos.length,fig_pos.height]);
-    a.plot2=plot(evapo_mmday(:,nt),...
+         ,[fig_pos.left+0.4,fig_pos.bottom,...
+          fig_pos.length-0.035,fig_pos.height]);
+    a.plot2=plot(x_matrix(1,:), evapo_mmday(:,nt),...
              'k-','linewidth',a.lw);hold off
     %a.plot1=plot(eslab(1,:),eslab(2,:),'cx','linewidth',a.lw);
     get(gca,'xtick');
     set(gca,'fontsize',10);
     xlabel('x','FontSize',a.fs);
     ylabel('Evt (mm/day)','FontSize',a.fs);
-    axis([0 inp.nn1+1 -0.5 40])
+    axis([0 1.2 0 inf])
+    
+    %% -------------  sub 3 velocity vector for each node  --------------
+    a.sub3=subplot('position'...
+         ,[fig_pos.left+0.4,fig_pos.bottom-0.64,...
+          fig_pos.length-0.035,fig_pos.height]);
+    vx_matrix = reshape(ele(nt).terms{vx_idx},[inp.nn1-1,inp.nn2-1]);
+    vy_matrix = reshape(ele(nt).terms{vy_idx},[inp.nn1-1,inp.nn2-1]);
+    a.plot3=quiver(x_ele_matrix,y_ele_matrix,vx_matrix,vy_matrix);hold off
+    %a.plot1=plot(eslab(1,:),eslab(2,:),'cx','linewidth',a.lw);
+    get(gca,'xtick');
+    set(gca,'fontsize',10);
+    xlabel('x (m)','FontSize',a.fs);
+    ylabel('Elevation (m)','FontSize',a.fs);
+    title('Velocity')
+    axis([0 1.2 0 0.4])
    
-
 %% -------- contour plot on Saturation ---------
-    a.sub5=subplot('position'...
-         ,[fig_pos.left,fig_pos.bottom-(fig_pos.height+0.07),...
-          fig_pos.length+0.1,fig_pos.height]);
-		  
+    a.sub4=subplot('position'...
+         ,[fig_pos.left,fig_pos.bottom-0.32,...
+          fig_pos.length,fig_pos.height]);		  
     % write pressure and conc in matrix form.
     s_matrix  = reshape(nod(nt).terms{s_idx},[inp.nn1,inp.nn2]);
-    contourf(x_matrix,y_matrix,s_matrix,'EdgeColor','none');hold on
+    a.plot4=contourf(x_matrix,y_matrix,s_matrix,'EdgeColor','none');hold off
 
 %    scatter(nod(1).terms{x_idx},nod(1).terms{y_idx},2,'filled','w');
     color = jet;
+    color = flipud(color);
     colormap(gca,color);
 	caxis([0 1])
     cbsal = colorbar;
@@ -128,17 +136,17 @@ yyaxis right
     set(gca,'fontsize',10);
     xlabel('x (m)','FontSize',a.fs);
     ylabel('Elevation (m)','FontSize',a.fs);
-    title('Saturation')
+    title('Saturation (-)')
 
 
 %% -------- contour plot on concentration ---------
-    a.sub6=subplot('position'...
-         ,[fig_pos.left,fig_pos.bottom-2*(fig_pos.height+0.07),...
-          fig_pos.length+0.1,fig_pos.height]);
+    a.sub5=subplot('position'...
+         ,[fig_pos.left+0.4,fig_pos.bottom-0.32,...
+          fig_pos.length,fig_pos.height]);
     
     % write pressure and conc in matrix form.
     c_matrix = reshape(nod(nt).terms{c_idx},[inp.nn1,inp.nn2]);    
-    contourf(x_matrix,y_matrix,c_matrix,'EdgeColor','none');hold on
+    a.plot5=contourf(x_matrix,y_matrix,c_matrix,'EdgeColor','none');hold off
 	
 %	scatter(nod(1).terms{x_idx},nod(1).terms{y_idx},2,'filled','w');
     color = jet;
@@ -152,7 +160,58 @@ yyaxis right
     xlabel('x (m)','FontSize',a.fs);
     ylabel('Elevation (m)','FontSize',a.fs);
     %axis([10, 40,9,10])
+    
+    %% -------- contour plot on temperature ---------
+    a.sub6=subplot('position'...
+         ,[fig_pos.left,fig_pos.bottom-0.64,...
+          fig_pos.length,fig_pos.height]);
+    
+    % write pressure and conc in matrix form.
+    temp_matrix = reshape(nod(nt).terms{temp_idx},[inp.nn1,inp.nn2]);    
+    a.plot6=contourf(x_matrix,y_matrix,temp_matrix-273.15,'EdgeColor','none');hold off
 	
+%	scatter(nod(1).terms{x_idx},nod(1).terms{y_idx},2,'filled','w');
+    color = jet;
+    colormap(gca,color);
+	caxis([20 40])
+    cbsal = colorbar;
+    cbsal.Label.String = 'Temperature (°C)';
+    get(gca,'xtick');
+    set(gca,'fontsize',10);
+    title('Temperature (°C)');
+    xlabel('x (m)','FontSize',a.fs);
+    ylabel('Elevation (m)','FontSize',a.fs);
+    %axis([10, 40,9,10])
+    
+    %% ------------- concentration profile at given x --------------
+    a.sub7=subplot('position'...
+         ,[fig_pos.left+0.77,fig_pos.bottom-0.32,...
+          0.15,fig_pos.height]);
+    c_profile = c_matrix(:,11:20:51);
+    a.plot7=plot(c_profile,y_matrix(:,11:20:51),'-','linewidth',a.lw);hold off
+    get(gca,'xtick');
+    set(gca,'fontsize',10);
+    set(gca,'yaxislocation','right');
+    xlabel('Concentration (-)','FontSize',a.fs);
+    legend('x=0.2 m','x=0.6 m','x=1.0 m','Location','southeast')
+    axis([0 0.3 0.2 0.4])
+    %% ------------- zoom in velocity --------------
+    a.sub7=subplot('position'...
+         ,[fig_pos.left+0.77,fig_pos.bottom-0.64,...
+          0.15,fig_pos.height]);
+    vx_matrix = reshape(ele(nt).terms{vx_idx},[inp.nn1-1,inp.nn2-1]);
+    vy_matrix = reshape(ele(nt).terms{vy_idx},[inp.nn1-1,inp.nn2-1]);
+    a.plot7=quiver(x_ele_matrix,y_ele_matrix,vx_matrix,vy_matrix);hold off
+    %a.plot1=plot(eslab(1,:),eslab(2,:),'cx','linewidth',a.lw);
+    get(gca,'xtick');
+    set(gca,'fontsize',10);
+    xlabel('x (m)','FontSize',a.fs);
+    ylabel('Elevation (m)','FontSize',a.fs);
+    title('Velocity')
+    axis([1. 1.2 0.2 0.35])
+    
+    
+    
 	F = getframe(gcf); % save the current figure
     writeVideo(mov,F);% add it as the next frame of the movie
 
